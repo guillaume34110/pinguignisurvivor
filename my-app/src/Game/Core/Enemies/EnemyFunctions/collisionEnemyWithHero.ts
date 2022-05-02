5ca9d53867875b637afb3e0d7c920c3188f6c2d7
maxime-rebaze <maximemoya@gmail.com>
2022-05-02 18:47:58 +0200

    mob

---
+++ my-app/src/Game/Core/Enemies/EnemyFunctions/collisionEnemyWithHero.ts
@@ -0,0 +1,10 @@
+import { Enemy, Hero } from "../../StartData/StartData"
+
+function collision(){}
+
+const collisionEnemyWithHero = (enemy: Enemy, hero: Hero): boolean => {
+    if (true) {
+        return true
+    }
+    else { return false }
+}
--- my-app/src/Game/Core/Enemies/enemies.ts
+++ my-app/src/Game/Core/Enemies/enemies.ts
@@ -1,1 +1,33 @@
-export const enemies = " "
+import { Enemy } from "../StartData/StartData"
+
+interface Enemies {
+    medusa: Enemy,
+    flozer: Enemy,
+}
+
+export const enemies: Enemies = {
+
+    medusa: {
+        type: "",
+        level: 1,
+        life: 1,
+        damages: 1,
+        speed: 1,
+        sprite: "",
+        x: 1,
+        y: 1,
+    },
+
+    flozer: {
+        type: "",
+        level: 1,
+        life: 1,
+        damages: 1,
+        speed: 1,
+        sprite: "",
+        x: 1,
+        y: 1,
+    }
+
+}
+
