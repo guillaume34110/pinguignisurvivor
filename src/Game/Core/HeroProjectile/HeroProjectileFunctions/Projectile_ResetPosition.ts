d1e24181a23fe5ab067ed50d4d28a4da81e7816f
guillaume34110 <gaillard.guillaum@gmail.com>
2022-11-29 16:44:09 +0100

    remodeling architecture

--- src/Game/Core/HeroProjectile/HeroProjectileFunctions/Projectile_ResetPosition.ts
+++
@@ -1,9 +0,0 @@
-import {HeroProjectile, HeroProjectileClass} from "../HeroProjectile";
-import {GameData} from "../../StartData/StartData";
-
-export const projectile_ResetPosition = (projectile : HeroProjectileClass , gameData : GameData)=> {
-    projectile.spriteBox.x =  gameData.hero.spriteBox.x
-    projectile.spriteBox.y =  gameData.hero.spriteBox.y
-    projectile.hitBox.x =  gameData.hero.spriteBox.x
-    projectile.hitBox.y = gameData.hero.spriteBox.y
-}
