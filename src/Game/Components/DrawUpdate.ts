734025b79d7baeb7bb0d4ce6645a184987187ef7
guillaume34110 <gaillard.guillaum@gmail.com>
2022-12-19 16:39:54 +0100

    update

--- src/Game/Components/DrawUpdate.ts
+++ src/Game/Components/DrawUpdate.ts
@@ -1,9 +1,9 @@
 import {GameData} from "../Core/StartData/StartData";
-import {mapBlocksRefresh} from "./mapBlocks/mapBlocks_Draw";
 import {items_Refresh} from "./Items/Items_Draw";
 import {creaturesRefresh} from "./Creatures/Creatures_Draw";
 import {hitBoxRefresh} from "./Hitboxs/Hitbox_Draw";
 import { lifeBarDraw } from "./LifeBar/LifeBar_Draw";
+import { mapBlocksRefresh } from "./MapBlocks/MapBlocks_Draw";
 
 export const draw = (gameData:GameData , targetGl : CanvasRenderingContext2D ) => {
     mapBlocksRefresh(gameData, targetGl)
